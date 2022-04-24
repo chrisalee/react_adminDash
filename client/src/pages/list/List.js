f52d6bcf34d394efc4177cf7efe3f66070a13eca
Chris Lee <christopher.lee222@yahoo.com>
2022-04-23 23:21:44 -0500

    datatable component data grid from material ui

--- client/src/pages/list/List.js
+++ client/src/pages/list/List.js
@@ -1,10 +1,16 @@
-import React from 'react';
 import './list.scss';
+import Sidebar from '../../components/sidebar/Sidebar';
+import Navbar from '../../components/navbar/Navbar';
+import Datatable from '../../components/datatable/Datatable';
 
 const List = () => {
   return (
     <div className='list'>
-      List
+      <Sidebar />
+      <div className="list__container">
+        <Navbar />
+        <Datatable />
+      </div>
     </div>
   )
 }
