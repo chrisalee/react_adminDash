7ccce68a0d1199b8bebfcb851b44b50cd7df5478
Chris Lee <christopher.lee222@yahoo.com>
2022-04-29 12:13:22 -0500

    delete function on datatable

--- client/src/components/datatable/Datatable.js
+++ client/src/components/datatable/Datatable.js
@@ -2,20 +2,28 @@ import "./datatable.scss";
 import { DataGrid } from "@mui/x-data-grid";
 import { userColumns, userRows } from "../../datatablesource";
 import { Link } from 'react-router-dom';
+import { useState } from "react";
 
 const Datatable = () => {
+
+  const [data, setData] = useState(userRows);
+
+  const handleDelete = (id) => {
+    setData(data.filter(item => item.id !== id))
+  }
+
   const actionColumn = [
     {
       field: "action",
       headerName: "Action",
       width: 200,
-      renderCell: () => {
+      renderCell: (params) => {
         return (
           <div className="cell__action">
             <Link to='/users/:id' style={{textDecoration: "none"}}>
               <div className="view__button">View</div>
             </Link>
-            <div className="delete__button">Delete</div>
+            <div className="delete__button" onClick={() => handleDelete(params.row.id)}>Delete</div>
           </div>
         );
       },
@@ -32,10 +40,10 @@ const Datatable = () => {
       </div>
       <DataGrid
         className="datagrid"
-        rows={userRows}
+        rows={data}
         columns={userColumns.concat(actionColumn)}
-        pageSize={9}
-        rowsPerPageOptions={[9]}
+        pageSize={6}
+        rowsPerPageOptions={[6]}
         checkboxSelection
       />
     </div>
